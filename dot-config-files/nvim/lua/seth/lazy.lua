86566ee73201e1b8866b28dff8d59a885f0282cb
Seth Horsley <isethi@me.com>
2024-04-23 13:55:21 +0200

    ✨ changes file names

--- dot-config-files/nvim/lua/seth/lazy.lua
+++ dot-config-files/nvim/lua/seth/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("seth.plugins")
+require("lazy").setup("seth.plugins", {
+  change_dection = {
+    notify = false,
+  }
+})
